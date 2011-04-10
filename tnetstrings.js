8f1027805401bb6abfd9ef3050fa660c5da3a71d
Alexander Solovyov <alexander@solovyov.net>
2011-04-10 21:56:22 +0200

    initial commit

---
+++ tnetstrings.js
@@ -0,0 +1,146 @@
+var tnetstrings = (function () {
+    this.parse_payload = function(data) {
+        if (!data) {
+            throw "Invalid data to parse, it's empty.";
+        }
+        var idx = data.indexOf(":");
+        if (idx === -1) {
+            throw "Missing netstring delimiter";
+        }
+
+        var length = parseInt(data.substr(0, idx), 10);
+        var payload = data.substr(idx + 1, length);
+        if (payload.length !== length) {
+            throw "Data is wrong length: " + length + " vs " + payload.length;
+        }
+
+        var type = data.substr(idx + length + 1, 1);
+        var extra = data.substr(idx + length + 2);
+        if (!type) {
+            throw "No payload type: " + extra;
+        }
+        return {payload: payload, type: type, extra: extra};
+    }
+
+
+    this.parse_list = function(data) {
+        if (!data) {
+            return [];
+        }
+        var result = [];
+        var p = this.parse(data);
+        result[result.length] = p.value;
+        while(p.extra) {
+            p = this.parse(p.extra);
+            result[result.length] = p.value;
+        }
+        return result;
+    }
+
+    this.parse_pair = function(data) {
+        var pk = this.parse(data);
+        if (!pk.extra) {
+            throw "Unbalanced dictionary store.";
+        }
+        var pv = this.parse(pk.extra);
+        if (!pk.value) {
+            throw "Got an invalid value, null not allowed.";
+        }
+        return {key: pk.value, value: pv.value, extra: pv.extra};
+    }
+
+    this.parse_dict = function(data) {
+        if (!data) {
+            return {};
+        }
+        var kv = parse_pair(data);
+        var result = {};
+        result[kv.key] = kv.value;
+        while (kv.extra) {
+            kv = parse_pair(kv.extra);
+            result[kv.key] = kv.value;
+        }
+        return result;
+    }
+
+    this.dump_object = function(data) {
+        var i, type, result = [];
+
+        if (data === null) {
+            return '0:~';
+        }
+        else if (Object.prototype.toString.apply(data) === '[object Array]') {
+            type = ']';
+            for (i = 0; i < data.length; i++) {
+                result[i] = dump(data[i]);
+            }
+        } else {
+            type = '}';
+            for (i in data) {
+                if (data.hasOwnProperty(i)) {
+                    result[result.length] = dump(i);
+                    result[result.length] = dump(data[i]);
+                }
+            }
+        }
+
+        var payload = result.join('');
+        return payload.length + ':' + payload + type;
+    }
+
+    this.parse = function(data) {
+        var value;
+        var p = this.parse_payload(data);
+        switch (p.type) {
+        case '#':
+            value = parseInt(p.payload, 10);
+            break;
+        case '!':
+            value = p.payload === 'true';
+            break;
+        case '~':
+            if (p.payload.length != 0)
+                throw "Payload must be 0 length for null.";
+            value = null;
+            break;
+        case ',':
+            value = p.payload;
+            break;
+        case '}':
+            value = this.parse_dict(p.payload);
+            break;
+        case ']':
+            value = this.parse_list(p.payload);
+            break;
+        default:
+            throw "Invalid payload type: " + p.type;
+        }
+        return {value: value, extra: p.extra};
+    }
+
+    this.dump = function(data) {
+        switch (typeof data) {
+        case 'number':
+            // return null for infinite numbers
+            if (!isFinite(data))
+                return dump(null);
+            var out = data.toString();
+            return out.length + ':' + out + '#';
+        case 'boolean':
+            var out = data.toString();
+            return out.length + ':' + out + '!';
+        case 'string':
+            return data.length + ':' + data + ',';
+        case 'object':
+            // object in js could be dict, list, null
+            return this.dump_object(data);
+        }
+    }
+
+    return this;
+
+    return {parse: parse, dump: dump,
+            _parse_payload: parse_payload, _parse_list: parse_list,
+            _parse_pair: parse_pair, _parse_dict: parse_dict,
+            _dump_object: dump_object};
+})();
