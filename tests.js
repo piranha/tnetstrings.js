90c1ab8898b7fd41b6959099ff0220926b437978
Gerard Paapu <sharkbrainguy@gmail.com>
2011-08-22 14:42:04 +1200

    added node.js powered tests

---
+++ tests.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+var tnetstrings = require('./tnetstrings'),
+    assert = require('assert');
+
+[
+    ['0:~', null],
+    ['5:false!', false],
+    ['4:test,', 'test'],
+    ['3:123#', 123],
+    ["16:5:hello,5:12345#}", {hello: 12345}],
+    ["32:5:hello,5:12345#5:hello,5:56789#]", ["hello", 12345, "hello", 56789]],
+    ["9:3.1415926^", 3.1415926]
+].forEach(function (test) {
+    assert.deepEqual( tnetstrings.parse(test[0]).value, test[1] );
+    assert.deepEqual( tnetstrings.dump(test[1]), test[0] );
+});
+
+console.log('tests done');
